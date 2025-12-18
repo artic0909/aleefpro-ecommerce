e3755a108ef5ba523c6d6c2b3eb066ca9db4b64e
artic0909 <alfag327@gmail.com>
2025-12-18 14:39:42 +0530

    r

--- resources/views/Admin/admin-custom-product-enquiry.blade.php
+++ resources/views/Admin/admin-custom-product-enquiry.blade.php
@@ -368,15 +368,7 @@
 
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
-                <footer class="footer">
-                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2024. Premium <a href="https://github.com/artic0909" target="_blank">Saklin admin template</a> - All rights reserved.</span>
-                        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Easy-To-Use & made with <i class="ti-heart text-danger ml-1"></i></span>
-                    </div>
-                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://github.com/artic0909" target="_blank">SaklinMustak</a></span>
-                    </div>
-                </footer>
+                
                 <!-- partial -->
             </div>
             <!-- main-panel ends -->
